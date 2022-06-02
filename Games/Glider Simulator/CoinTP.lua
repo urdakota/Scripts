e7571e5ce01f9dedc18c5a9dabcb7557b3e57256
Dakota <95160810+urdakota@users.noreply.github.com>
2022-06-02 08:59:01 -0400

    Update CoinTP.lua

--- Games/Glider Simulator/CoinTP.lua
+++ Games/Glider Simulator/CoinTP.lua
@@ -1,5 +1,3 @@
-while wait(10) do
-    for i,v in pairs(game:GetService("Workspace").Collectables:GetChildren()) do
-        v.Ring.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame  
-    end
+for i,v in pairs(game:GetService("Workspace").Collectables:GetChildren()) do
+   v.Ring.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame  
 end

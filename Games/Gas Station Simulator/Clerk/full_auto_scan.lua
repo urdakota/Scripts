3cb0db3db0a891ec2bc4d08eac38c8af14b011e4
Dakota <95160810+urdakota@users.noreply.github.com>
2022-06-03 07:57:15 -0400

    Update full_auto_scan.lua

--- Games/Gas Station Simulator/Clerk/full_auto_scan.lua
+++ Games/Gas Station Simulator/Clerk/full_auto_scan.lua
@@ -1,21 +1,25 @@
 for i,v in pairs(workspace.Checkouts:GetChildren()) do
-   if #v.Items:GetChildren() > 0 then
-     for x,d in pairs(v.Items:GetChildren()) do
-        if d:FindFirstChild("Root") and d.Root:FindFirstChild("Scan") and d.Root:FindFirstChild("Scan").Enabled then 
-           fireproximityprompt(d.Root.Scan);
-           wait(1);
-           wait(2.5);
-        end
-     end
-   end
-   v.Items.ChildAdded:Connect(function(item)
-      if item:WaitForChild("Root") and item:FindFirstChild("Root"):WaitForChild("Scan") then
-        item:FindFirstChild("Root"):WaitForChild("Scan").PromptShown:Connect(function()
-            for i,v in pairs(game.Players.LocalPlayer.Character.Humanoid:GetPlayingAnimationTracks()) do
-               v.Stopped:Wait()
+    if #v.Items:GetChildren() > 0 then
+        for x,d in pairs(v.Items:GetChildren()) do
+            if d:WaitForChild("Root") and d:FindFirstChild("Root"):WaitForChild("Scan") then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = d.Root.CFrame + Vector3.new(0,2.5,0)
+                wait(2.5)
+                repeat wait() until d == nil or d.Root.Scan.Enabled
+                fireproximityprompt(d.Root.Scan);
+                wait(1.8)
             end
-            fireproximityprompt(item:FindFirstChild("Root"):WaitForChild("Scan"))
-        end)
-      end
+        end
+    end
+    v.Items.ChildAdded:Connect(function(item)
+        if item:WaitForChild("Root") and item:FindFirstChild("Root"):WaitForChild("Scan") then
+            repeat 
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = item.Root.CFrame + Vector3.new(0,2.5,0)
+                wait(2.5)
+                if item and item:WaitForChild("Root") and item:FindFirstChild("Root"):WaitForChild("Scan") and item.Root.Scan.Enabled then
+                    fireproximityprompt(item:FindFirstChild("Root"):WaitForChild("Scan"))
+                    wait(1.8)
+                end
+            until item == nil 
+        end
    end)
 end

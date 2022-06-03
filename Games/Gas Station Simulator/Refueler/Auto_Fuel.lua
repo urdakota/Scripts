304d9b24046bb8bc5dbad1c70a871565c34bc949
Dakota <95160810+urdakota@users.noreply.github.com>
2022-06-03 01:32:49 -0400

    Update Auto_Fuel.lua

--- Games/Gas Station Simulator/Refueler/Auto_Fuel.lua
+++ Games/Gas Station Simulator/Refueler/Auto_Fuel.lua
@@ -5,11 +5,6 @@ for ib,car in pairs(workspace:GetChildren()) do
       if car:FindFirstChild("Lid") and car:FindFirstChild("Lid"):FindFirstChild("Refuel") then
          if (tonumber(string.sub(car.Root.Pump.Value.Screen.FuelUI.Storage.Text, 1, -2)) > tonumber(string.sub(car.Lid:FindFirstChild("Refuel").ObjectText,7,-2))) then
             fireproximityprompt(car.Lid:FindFirstChild("Refuel"))
-        else
-           if BuyFuel then
-            game:GetService("ReplicatedStorage").Remote:FireServer("BuyItem", "Syntin Petrol Co", "Gasoline 87", 1, "Station")
-            fireproximityprompt(car.Lid:FindFirstChild("Refuel"))
-           end
         end
       end
    end
